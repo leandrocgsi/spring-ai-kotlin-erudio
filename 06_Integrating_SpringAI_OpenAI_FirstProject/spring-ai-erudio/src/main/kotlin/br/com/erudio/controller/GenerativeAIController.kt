a5cbca0a80d596c1b8d1cfad66358cf5e82e77eb
leandrocgsi <leandrocgsi@gmail.com>
2025-04-01 12:19:04 -0300

    Chat, Recipe Generation, and Images with Spring AI, OpenAI and ChatGPT

--- 06_Integrating_SpringAI_OpenAI_FirstProject/spring-ai-erudio/src/main/kotlin/br/com/erudio/controller/GenerativeAIController.kt
+++ 06_Integrating_SpringAI_OpenAI_FirstProject/spring-ai-erudio/src/main/kotlin/br/com/erudio/controller/GenerativeAIController.kt
@@ -1,11 +1,16 @@
 package br.com.erudio.controller
 
 import br.com.erudio.service.ChatService
+import br.com.erudio.service.RecipeService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class GenerativeAIController (private val chatService: ChatService) {
+class GenerativeAIController (
+        private val chatService: ChatService,
+        private val recipeService: RecipeService
+    ) {
 
     @GetMapping("ask-ai")
     fun askAi(prompt: String?): String? {
@@ -15,5 +20,18 @@ class GenerativeAIController (private val chatService: ChatService) {
     @GetMapping("ask-ai-options")
     fun askAiWithOptions(prompt: String?): String? {
         return chatService.getResponseWithOptions(prompt);
+    }
+
+    @GetMapping("recipe-creator")
+    fun generateRecipe(
+        @RequestParam ingredients: String,
+        @RequestParam(defaultValue = "any") cuisine: String,
+        @RequestParam(defaultValue = "none") dietaryRestrictions: String): String? {
+
+        if (ingredients.isNullOrBlank()) {
+            throw IllegalArgumentException("The 'ingredients' parameter is required and cannot be empty!")
+        }
+
+        return recipeService.createRecipe(ingredients, cuisine, dietaryRestrictions);
     }
 }
